fd7fdfe047f115eb125b94c093414893ae25e6dc
Chenghong Lin <chenghonglin2020@u.northwestern.edu>
2019-01-16 15:52:00 -0600

    Modify code & show product title and price statically

--- src/App.js
+++ src/App.js
@@ -2,30 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class ProductCategoryRow extends React.Component{
-    render(){
-        const category = this.props.category;
-        return(
-            <tr>
-                <th colSpan="2">
-                    {category}
-                </th>
-            </tr>
-        )
-    }
-}
-
 class ProductRow extends React.Component{
     render(){
         const product = this.props.product;
-        const name = product.stocked ? product.name :
-            <span style={{color: 'red'}}>
-                {product.name}
-            </span>;
 
         return(
             <tr>
-                <td>{name}</td>
+                <td>{product.title}</td>
                 <td>{product.price}</td>
             </tr>
         )
@@ -35,24 +18,14 @@ class ProductRow extends React.Component{
 class ProductTable extends React.Component {
     render(){
         const rows = [];
-        let lastCategory = null;
 
         this.props.products.forEach((product) => {
-            if(product.category !== lastCategory){
-                rows.push(
-                    <ProductCategoryRow
-                        category={product.category}
-                        key={product.category}
-                    />
-                )
-            }
             rows.push(
                 <ProductRow
                     product={product}
                     key={product.name}
                 />
             );
-            lastCategory = product.category;
         });
 
         return(
@@ -63,39 +36,8 @@ class ProductTable extends React.Component {
                         <th>Price</th>
                     </tr>
                 </thead>
-                <tbody>${rows}</tbody>
+                <tbody>{rows}</tbody>
             </table>
-        )
-    }
-}
-
-class ClothPic extends Component{
-
-}
-
-class ClothName extends Component{
-
-}
-
-class ClothPrice extends Component{
-
-}
-
-const PRODUCTS = [
-    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-];
-
-class FilterableProductTable extends Component{
-    render(){
-        return (
-            <div>
-                <ProductTable products={this.props.products} />
-            </div>
         )
     }
 }
@@ -106,7 +48,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       data : null
-    };
+    }
   }
 
   componentDidMount() {
@@ -117,12 +59,15 @@ class App extends React.Component {
   }
 
   render(){
+      // why state.data can be null???
       if(this.state.data){
           console.log("data => ", this.state.data);
+          return (
+              <ProductTable products={this.state.data}/>
+          );
+      }else{
+          return <h1>Loading...</h1>
       }
-      return (
-          <FilterableProductTable products={PRODUCTS}/>
-      );
   }
 }
 
